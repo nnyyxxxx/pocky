9a47456366d3c70e07678443a4d6fabb198c436d
nnyyxxxx <nnyyxxxx@protonmail.com>
2025-03-02 23:17:46 -0500

    feat: render a still cursor

--- kernel/src/shell/graphics.cpp
+++ kernel/src/shell/graphics.cpp
@@ -10,9 +10,16 @@
 bool in_graphics_mode = false;
 
 constexpr uint8_t SCAN_ESC = 0x01;
+constexpr uint16_t CURSOR_X = 40;
+constexpr uint16_t CURSOR_Y = 12;
 
 void graphics_initialize() {
     // no-op
+}
+
+void render_cursor() {
+    const uint16_t index = CURSOR_Y * VGA_WIDTH + CURSOR_X;
+    VGA_MEMORY[index] = vga_entry(30, vga_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK));
 }
 
 void enter_graphics_mode() {
@@ -27,6 +34,8 @@ void enter_graphics_mode() {
 
     outb(VGA_CTRL_PORT, 0x0A);
     outb(VGA_DATA_PORT, 0x20);
+
+    render_cursor();
 
     while (in_graphics_mode) {
         if (inb(KEYBOARD_STATUS_PORT) & 1) {
